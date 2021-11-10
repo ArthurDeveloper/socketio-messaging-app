ec180e9496eb54b4a598102ddd37ae1308560078
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-11-10 14:58:18 -0300

    feat: Added cors to socketio server

--- src/io.ts
+++ src/io.ts
@@ -1,6 +1,11 @@
 import { Server } from 'socket.io';
 import server from './server';
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: 'https://socketio-messaging-app.herokuapp.com/',
+        methods: ['GET', 'POST']
+    }
+});
 
 export default io;
