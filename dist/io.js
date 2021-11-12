118775e36d4057659bc2920af10b6d73b94911e7
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2021-11-11 21:34:49 -0300

    fix: Deleted useless dist directory that i accidentally added
    Now, i put it's name inside .gitignore file, to prevent i make this mistake again in the future.

--- dist/io.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var socket_io_1 = require("socket.io");
-var server_1 = __importDefault(require("./server"));
-var io = new socket_io_1.Server(server_1.default);
-exports.default = io;
